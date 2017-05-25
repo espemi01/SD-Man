fd396f83c1e2d3e0d6779e806e8786426d79d719
Michael Espey <espemi01@luther.edu>
2017-05-25 09:28:40 -0500

    Initial Commit

---
+++ SD Man/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SD Man
+//
+//  Created by Michael Espey on 5/25/17.
+//  Copyright © 2017 MAEspey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
